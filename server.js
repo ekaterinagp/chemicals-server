d7c2fe0c3f991370e17e1e158e9174ad92d5de81
ekaterinagp <katya.koriakina@gmail.com>
2020-05-27 17:44:14 +0200

    port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 const cors = require("cors");
 // let allowedOrigins = ["http://localhost:3000", "http://ekaterinagp.dk"];
